a436ab629b5230fce87eb29870a9fd5f49566f22
Omid Ghahramani <omid.12.ghari@gmail.com>
2022-08-31 23:06:51 +0000

    connect function module.export

---
+++ client.js
@@ -0,0 +1,18 @@
+const net = require("net");
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",// IP address here,
+    port: 50541// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  // handles incoming data and logs it for player 
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+  return conn;
+};
+
+module.exports = connect;
