3da9f298e43d5f89a020b1f7ff8cc67ee496c400
Omid Ghahramani <omid.12.ghari@gmail.com>
2022-09-01 03:44:27 +0000

    movement input keys added

--- input.js
+++ input.js
@@ -1,7 +1,11 @@
 // setup interface to handle user input from stdin
+//const { connect } = require('http2');
+//const connect = require('./client');
+// Stores the active TCP connection object.
+let connection;
 
-
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -11,6 +15,21 @@ const setupInput = function () {
     // your code here
     if (key === '\u0003') {
       process.exit();
+    }
+    if (key === 'w') {
+      connection.write('Move: up')
+    }
+
+    if (key === 'a') {
+      connection.write('Move: left')
+    }
+
+    if (key === 's') {
+      connection.write('Move: down')
+    }
+
+    if (key === 'd') {
+      connection.write('Move: right')
     }
   };
 
