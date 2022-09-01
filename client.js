3271b66547ca45ba64ed02c6e531e98efe811d1f
Omid Ghahramani <omid.12.ghari@gmail.com>
2022-09-01 03:44:11 +0000

    movement added

--- client.js
+++ client.js
@@ -11,6 +11,7 @@ const connect = function () {
   conn.on("connect", () => {
     console.log('Successfully connected to game server');
     conn.write('Name: OG');
+    //conn.write('Move: up');
   });
 
   conn.on("data", (data) => {
