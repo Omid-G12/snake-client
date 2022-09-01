2d71d34cebe857d2324217d1a7a8cc557d174afb
Omid Ghahramani <omid.12.ghari@gmail.com>
2022-09-01 00:43:01 +0000

    ^C for exit input added

--- play.js
+++ play.js
@@ -1,4 +1,23 @@
 const connect = require('./client');
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = function (key) {
+    // your code here
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
+
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
 // establishes a connection with the game server
 console.log("Connecting ...");
 connect();
+setupInput();
