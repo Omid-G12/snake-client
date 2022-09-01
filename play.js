579b2dd79696d120df18b8c1124e0f4b1b2642c3
Omid Ghahramani <omid.12.ghari@gmail.com>
2022-09-01 00:49:14 +0000

    refactored with input module

--- play.js
+++ play.js
@@ -1,22 +1,6 @@
 const connect = require('./client');
-// setup interface to handle user input from stdin
+const setupInput = require('./input');
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  const handleUserInput = function (key) {
-    // your code here
-    if (key === '\u0003') {
-      process.exit();
-    }
-  };
-
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
 // establishes a connection with the game server
 console.log("Connecting ...");
 connect();
